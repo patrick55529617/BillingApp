a7d893a6b0f2dfeab143aaf8eab378736370048c
Patrick Hu <patrick55529617@gmail.com>
2020-03-31 11:24:23 +0800

    Use button to delete ver1.

--- BillingApp/BillingInfo/admin.py
+++ BillingApp/BillingInfo/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from BillingInfo.models import BillInfo
 
-# Register your models here.
+admin.site.register(BillInfo)
--- BillingApp/BillingInfo/views.py
+++ BillingApp/BillingInfo/views.py
@@ -3,9 +3,10 @@ from django.views import View
 from BillingInfo.models import BillInfo
 from django.http import HttpResponse, HttpResponseRedirect
 import datetime
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
-
-class AddBillingPage(View):
+class AddBillingPage(LoginRequiredMixin, View):
     def get(self, request):
         return render(request, 'add_billing.html')
 
@@ -16,13 +17,20 @@ class AddBillingPage(View):
 
         bill_info = BillInfo(content=content,
                              amount=amount,
-                             event_time=event_time)
+                             event_time=event_time,
+                             owner=request.user)
         bill_info.save()
 
         return HttpResponseRedirect("/test_show_all/")
 
+def DeleteBilling(request, id):
+    deleted_bill = BillInfo.objects.get(id=id)
+    deleted_bill.delete()
+    return HttpResponseRedirect("/test_show_all/")
 
+@login_required
 def test_show_all(request):
-    billing_list = BillInfo.objects.all().order_by('-event_time')
+    billing_list = BillInfo.objects.filter(
+        owner=request.user).order_by('-event_time')
     return render(request, 'test_show_all.html', {
         'billing_list': billing_list})
