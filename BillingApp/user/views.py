d6787ab01e69d9cb38d8e1c89f6ee5ae86ca898a
Patrick Hu <patrick55529617@gmail.com>
2020-04-02 11:30:32 +0800

    1. Add 'multiple delete' function. 2. Add logout function. 3. Add a button in '/test_show_all/' to link the '/add_billing/ page. 4. Improve the UI of page '/test_show_all/'.

--- BillingApp/user/views.py
+++ BillingApp/user/views.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.views import LoginView as AuthLoginView
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.contrib.auth import logout
 
 class LoginView(AuthLoginView):
     template_name = 'registration/login.html'
+
+def Logout(request):
+    logout(request)
+    return HttpResponseRedirect('/user/login')
