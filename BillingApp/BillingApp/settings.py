63f7fa94006fd22d037285a1a09c28d7abd3d891
Patrick Hu <patrick55529617@gmail.com>
2020-04-07 14:08:25 +0800

    Add static/css/main.css to manage css setting.

--- BillingApp/BillingApp/settings.py
+++ BillingApp/BillingApp/settings.py
@@ -113,6 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'templates/static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    ('css', os.path.join(STATIC_ROOT, 'css')),
+)
